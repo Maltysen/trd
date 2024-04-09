cdbc3a8584492153387555ed96717aff1bea08e4
Maltysen <firnen123@gmail.com>
2024-04-09 14:06:28 -0400

    fix indentation

--- code/bitwise_convolution.cpp
+++ code/bitwise_convolution.cpp
@@ -1,15 +1,15 @@
 // Fast Bitwise Convolution
 void FST(vl& a, bool in) {
-	for (int n = a.size(), step = 1; step < n; step *= 2) {
-		for (int i = 0; i < n; i += 2 * step) for(int j=i; j<i+step; ++j) {
-			ll &u = a[j], &v = a[j + step];
+    for (int n = a.size(), step = 1; step < n; step *= 2) {
+        for (int i = 0; i < n; i += 2 * step) for(int j=i; j<i+step; ++j) {
+            ll &u = a[j], &v = a[j + step];
             ll ou=u, ov=v;
             u = (ou+ov)%M, v=(ou-ov+M)%M; // XOR
             if (inv) u=(ov-ou+M)%M, v=ou; else u=ov, v=(ou+ov)%M; // AND
             if (inv) u=ov, v=(ou-ov+M)%M; else u=(ou+ov)%M, v=ou; // OR
-		}
-	}
+        }
+    }
     ll as = inv(a.size());
-	if (in) for (ll& x : a) ( x *= as )%=M; // XOR only
+    if (in) for (ll& x : a) ( x *= as )%=M; // XOR only
 }
 
